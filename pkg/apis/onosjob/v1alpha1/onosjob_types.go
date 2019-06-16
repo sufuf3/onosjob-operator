2274f5c77066fd4d32d9aac7ca34ef7dae8ed4b9
Samina Fu <sufuf3@gmail.com>
2019-06-16 14:17:58 +0000

    Update CR & specs

--- pkg/apis/onosjob/v1alpha1/onosjob_types.go
+++ pkg/apis/onosjob/v1alpha1/onosjob_types.go
@@ -10,19 +10,46 @@ import (
 // ONOSJobSpec defines the desired state of ONOSJob
 // +k8s:openapi-gen=true
 type ONOSJobSpec struct {
-	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
-	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
-	// Add custom validation using kubebuilder tags: https://book.kubebuilder.io/beyond_basics/generating_crd.html
+	ControllerIp   string        `json:"controllerIp"`
+	ControllerPort string        `json:"controllerPort"`
+	Hosts          []Hosts       `json:"hosts" validate:"-"`
+	FlowsDevice    []FlowsDevice `json:"flowsDevice" validate:"-"`
 }
 
 // ONOSJobStatus defines the observed state of ONOSJob
 // +k8s:openapi-gen=true
 type ONOSJobStatus struct {
-	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
-	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
-	// Add custom validation using kubebuilder tags: https://book.kubebuilder.io/beyond_basics/generating_crd.html
 }
 
+type Hosts struct {
+	Mac         string          `json:"mac"`
+	Vlan        string          `json:"vlan"`
+	IpAddresses []string        `json:"ipAddresses"`
+	Locations   []HostLocations `json:"locations"`
+}
+
+type HostLocations struct {
+	ElementId string `json:"elementId"`
+	Port      string `json:"port"`
+}
+
+type FlowsDevice struct {
+	Deviceid     string                    `json:"deviceid"`
+	Priority     int32                     `json:"priority"`
+	IsPermanent  bool                      `json:"isPermanent"`
+	Timeout      int32                     `json:"timeout"`
+	Instructions []FlowsDeviceInstructions `json:"instructions"`
+	Criteria     []FlowsDeviceCriteria     `json:"criteria"`
+}
+
+type FlowsDeviceInstructions struct {
+	Type string `json:"type"`
+	Port int32  `json:"port"`
+}
+type FlowsDeviceCriteria struct {
+	Type    string `json:"type"`
+	EthType string `json:"ethType"`
+}
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
 
 // ONOSJob is the Schema for the onosjobs API
